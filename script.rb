b959bd5e3a8b8c98b53bc2fb0da6d7aca6f6cc6e
iarunpaul <iarunpaul@yahoo.com>
2021-05-10 18:01:14 +0530

    Added Gemfile

--- script.rb
+++ script.rb
@@ -2,7 +2,7 @@ require './Classes/Cinema.rb'
 require './Classes/Movie.rb'
 require 'pry'
 
-# binding.pry
+binding.pry
 p cobble_hill = Cinema.new("Cobble Hill Cinema", "Court St., Brooklyn.")
 p spider_man = Movie.new("Amazing Spiderman", "9:00 PM")
 p cobble_hill.add_movie(spider_man)
